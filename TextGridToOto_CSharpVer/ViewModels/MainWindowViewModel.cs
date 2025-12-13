4d38a886226042a9ea50964f57e96e41c13e6cb8
Isogelun <isogelun@gmail.com>
2025-12-14 02:38:12 +0800

    添加项目文件。

---
+++ TextGridToOto_CSharpVer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TextGridToOto_CSharpVer.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ TextGridToOto_CSharpVer/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TextGridToOto_CSharpVer.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
